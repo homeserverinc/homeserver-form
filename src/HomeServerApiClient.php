a74c16e18176682a6066af500ef582a6ea7177cb
marcioelias <marcioelias@gmail.com>
2019-01-26 02:06:00 -0200

    Fix domain value

--- src/HomeServerApiClient.php
+++ src/HomeServerApiClient.php
@@ -17,8 +17,7 @@ class HomeServerApiClient {
      *
      * @var string
      */
-    //protected $domain = 'https://homeserverinc.com';
-    protected $domain = 'http://localhost:8000';
+    protected $domain = 'https://homeserverinc.com';
 
     /**
      * Username used to access the API
